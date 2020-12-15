da9ca3eeadba194194252df5daf61c2afcab2c34
Benoit BERTRAND <benoit.bertrand@live.fr>
2020-12-16 00:27:08 +0100

    Add Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularTestNumeroDeux';
+  title = 'Benoit fait des tests avec Angular';
 }
